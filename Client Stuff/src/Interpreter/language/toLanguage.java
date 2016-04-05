f4b2d5d8487a9136d28d0b87c76e72fcd6a4d659
Wolframhart2009 <tue97337@temple.edu>
2016-04-04 20:31:28 -0400

    documentation in EndLanguage interface

--- Client Stuff/src/Interpreter/language/toLanguage.java
+++ Client Stuff/src/Interpreter/language/toLanguage.java
@@ -2,13 +2,25 @@ package interpreter.language;
 
 
 public interface toLanguage {
-	
-	public int Translate(String s);
-        /*Returns an error code
+    
+	/*Input: Received String event from the phone. 
+         Output: Returns an error code. Allows for outside handling of errors in the interpreter stage.
           0 - Nothing went wrong
           1 - Script Execution error
+          2 - API Issue
+          3 - General Issue (Placeholder for more specific error codes)
+          This will translate the inputed signal into the language's required format and then execute it 
+          (whether by calling an api or by using a script). 
         */
+	public int Translate(String s);
         
+        /*Input: Nothing
+          Output: Path of implemented outside methods or scripts
+        */
 	public String getHeader();
+        
+        /*Input: Nothing
+          Output: Name of implemented language
+        */
         public String toString();
 }
